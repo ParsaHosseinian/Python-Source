688d40ef2223cec0ad1a7603441bf8f1b2a089af
Parsa <Parsa16.pmh@gmail.com>
2023-05-04 14:44:04 +0430

    variables.py added with introductions

---
+++ introduction of python basic concepts/variables.py
@@ -0,0 +1,15 @@
+# In Python, we can store objects in variables.
+# they can hold some sort of value which we can access and change anytime we want
+
+greetings = "Hello World"
+print (greetings)
+
+name = "Amirhossein"
+print ("My name is", name)
+
+age = 50
+print ("I am", age, "years old")
+
+hungry = True
+thirsty = True
+print (hungry and thirsty) # prints True

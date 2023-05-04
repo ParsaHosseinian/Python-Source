b37ced5d923c0a21dea33ade8ea9e54b6c5518be
Parsa <107501983+ParsaHoseiniyan@users.noreply.github.com>
2023-05-04 13:22:35 +0330

    first basic types python file added

---
+++ basictypes/basictypes.py
@@ -0,0 +1,7 @@
+# First off, let's start with the basic basics. In Python, each object /
+# thing has a type. For example, a word like
+
+print ("Hello World!") # This is a string
+print (3) # This is an integer
+print (4.5) # This is a float - a number with a decimal point
+print ("I am", 50, "years old") # We can print multiple things by separating by commas

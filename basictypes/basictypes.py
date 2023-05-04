dfe6a8ff333bfbe170d7e0955fdadf5ec018ca7c
Parsa <107501983+ParsaHoseiniyan@users.noreply.github.com>
2023-05-04 13:25:59 +0330

    Update basictypes.py

--- basictypes/basictypes.py
+++ basictypes/basictypes.py
@@ -5,3 +5,8 @@ print ("Hello World!") # This is a string
 print (3) # This is an integer
 print (4.5) # This is a float - a number with a decimal point
 print ("I am", 50, "years old") # We can print multiple things by separating by commas
+
+# We can check the type of an object using the type() 
+print (type("Hello World!")) #"str" means string
+print (type(3)) #"int" means integer
+print (type(4.5)) #"float" means a number with a decimal

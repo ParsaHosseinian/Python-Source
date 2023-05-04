7f90ce81acbb91c282d8c0004f435f9bc2956d15
Parsa <Parsa16.pmh@gmail.com>
2023-05-04 16:58:25 +0430

    class introductions added to basic concepts

---
+++ introduction of python basic concepts/class.py
@@ -0,0 +1,31 @@
+class MyClass:
+  x = 5
+
+
+p1 = MyClass()
+print(p1.x)
+
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("amirhossein", 22)
+
+print(p1.name)
+print(p1.age)
+
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()

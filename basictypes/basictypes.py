f60d42c51f0d52c0e5cc7a393c2d4d067c418ba6
Parsa <107501983+ParsaHoseiniyan@users.noreply.github.com>
2023-05-04 13:27:13 +0330

    boolean explanations aded

--- basictypes/basictypes.py
+++ basictypes/basictypes.py
@@ -10,3 +10,21 @@ print ("I am", 50, "years old") # We can print multiple things by separating by 
 print (type("Hello World!")) #"str" means string
 print (type(3)) #"int" means integer
 print (type(4.5)) #"float" means a number with a decimal
+
+# Another basic type in Python is the boolean. 
+# It can be used with logical operators like AND or OR
+
+print (True and False) # This is False
+print (True or False)  # This is True
+print (True and True)  # This is True
+print (False or False) # This is False
+print("===========")
+
+print('1 == 0: {}'.format(1 == 0))
+
+print('1 != 0: {}'.format(1 != 0))
+print('1 > 0: {}'.format(1 > 0))
+print('1 > 1: {}'.format(1 > 1))
+
+print('1 >= 0: {}'.format(1 >= 0))
+print('1 >= 1: {}'.format(1 >= 1))

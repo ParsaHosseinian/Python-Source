d1ca039fa4d17e315eaa31d144f8165a8b56347c
Parsa <Parsa16.pmh@gmail.com>
2023-05-06 00:03:18 +0430

    bitwise operators explain added to basic concepts

---
+++ introduction of python basic concepts(Harvard)/bitwise-operators.py
@@ -0,0 +1,13 @@
+# bitwise operators:
+
+# &  AND
+
+# |  OR 
+
+# ^  XOR                   (Sets each bit to 1 if only one of two bits is 1)
+
+# ~  NOT                   (Inverts all the bits)
+
+# << Zero fill left shift  (Shift left by pushing zeros in from the right and let the leftmost bits fall off)
+
+# >> Signed right shift    (Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off)

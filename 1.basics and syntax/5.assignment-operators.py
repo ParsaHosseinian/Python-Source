79e08851d45fd66915b5aaa5d6c551f605640a49
Parsa <Parsa16.pmh@gmail.com>
2025-02-20 16:44:27 +0330

    day 2 concepts added to the booklet

---
+++ 1.basics and syntax/5.assignment-operators.py
@@ -0,0 +1,27 @@
+# assignment operators with their examples:
+
+# = 
+
+# +=    # example: x += 3 (x = x + 3) (All the other assignment operators work like this.)
+
+# -=
+
+# *=
+
+# /=
+
+# %=
+
+# //=
+
+# **=
+
+# &=
+
+# |=
+
+# ^=
+
+# >>=
+
+# <<=
